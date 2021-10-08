6d0973719e3219a5cb92174df35c0aee53050c4e
Your Name <you@example.com>
2021-10-08 16:36:10 +0300

    removing uneeded things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Nav from "./components/nav/Nav";
 import ChatBody from "./components/chatBody/ChatBody";
 
 function App() {
   return (
     <div className="__main">
-      <Nav />
       <ChatBody />
     </div>
   );
