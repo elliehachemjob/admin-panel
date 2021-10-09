2b65c01d26451c7c6ec10fa93872d5eae79be3e2
Your Name <you@example.com>
2021-10-09 12:15:39 +0300

    finished last component

--- src/components/chatContent/ChatContent.js
+++ src/components/chatContent/ChatContent.js
@@ -1,12 +1,12 @@
-import React, { Component, useState, createRef, useEffect } from "react";
+import React, { Component, useState, createRef, useEffect,useRef } from "react";
 
 import "./chatContent.css";
 import Avatar from "../chatList/Avatar";
 import ChatItem from "./ChatItem";
 
-export default class ChatContent extends Component {
-  messagesEndRef = createRef(null);
-  chatItms = [
+export default function ChatContent (props) {
+  const messagesEndRef = useRef(null);
+  const chatItms = [
     {
       key: 1,
       image:
@@ -35,65 +35,50 @@ export default class ChatContent extends Component {
       type: "",
       msg: "Awesome these days.",
     },
-    {
-      key: 5,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
-      type: "other",
-      msg: "Finally. What's the plan?",
-    },
-    {
-      key: 6,
-      image:
-        "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
-      type: "",
-      msg: "what plan mate?",
-    },
-    {
-      key: 7,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
-      type: "other",
-      msg: "I'm taliking about the tutorial",
-    },
+  
   ];
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      chat: this.chatItms,
-      msg: "",
-    };
-  }
+  
 
-  scrollToBottom = () => {
-    this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+     const [chat, setChat] = useState(chatItms)
+     const [msg, setMsg] = useState("")
+
+
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
-  componentDidMount() {
+
+
+
+  useEffect(() => {
     window.addEventListener("keydown", (e) => {
       if (e.keyCode == 13) {
-        if (this.state.msg != "") {
-          this.chatItms.push({
+        if (msg != "") {
+          chatItms.push({
             key: 1,
             type: "",
-            msg: this.state.msg,
+            msg: msg,
             image:
               "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
           });
-          this.setState({ chat: [...this.chatItms] });
-          this.scrollToBottom();
-          this.setState({ msg: "" });
+          setChat({ chat: [...chatItms] });
+          scrollToBottom();
+          setMsg({ msg: "" });
         }
       }
     });
-    this.scrollToBottom();
-  }
-  onStateChange = (e) => {
-    this.setState({ msg: e.target.value });
+    scrollToBottom();
+  }, []);
+  
+
+
+  const onStateChange = (e) => {
+    setMsg({ msg: e.target.value });
   };
 
-  render() {
+  
     return (
       <div className="main__chatcontent">
         <div className="content__header">
@@ -114,7 +99,7 @@ export default class ChatContent extends Component {
         </div>
         <div className="content__body">
           <div className="chat__items">
-            {this.state.chat.map((itm, index) => {
+            {chat.map((itm, index) => {
               return (
                 <ChatItem
                   animationDelay={index + 2}
@@ -125,7 +110,7 @@ export default class ChatContent extends Component {
                 />
               );
             })}
-            <div ref={this.messagesEndRef} />
+            <div ref={messagesEndRef} />
           </div>
         </div>
         <div className="content__footer">
@@ -133,8 +118,8 @@ export default class ChatContent extends Component {
             <input
               type="text"
               placeholder="Type a message here"
-              onChange={this.onStateChange}
-              value={this.state.msg}
+              onChange={onStateChange}
+              value={msg}
             />
             <button className="btnSendMsg" id="sendMsgBtn">
               <i className="fa fa-paper-plane"></i>
@@ -143,5 +128,5 @@ export default class ChatContent extends Component {
         </div>
       </div>
     );
-  }
+  
 }
